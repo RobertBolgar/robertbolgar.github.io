bea87e59c4612b68d652e340560351688ff354df
Robert Bolgar <100732701+RobertBolgar@users.noreply.github.com>
2024-02-29 11:49:57 -0600

    Update app.js

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,16 +1,15 @@
 // app.js
-import { initializeWeb3 } from './web3Setup.js';
-import { setupContracts } from './contractsSetup.js';
-import { setupEventListeners } from './eventListeners.js';
+import { initializeWeb3 } from './js/web3Setup.js';
+import { setupContracts } from './js/contractsSetup.js';
+import { setupEventListeners } from './js/eventListeners.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     try {
         const provider = await initializeWeb3();
         const { nftContract, affiliateContract } = await setupContracts(provider);
-        setupEventListeners(nftContract, affiliateContract);
-        // Any other initialization code
+        setupEventListeners(nftContract, affiliateContract, provider);
+        // Any other initialization code goes here
     } catch (error) {
         console.error("Initialization failed:", error.message);
     }
 });
-
