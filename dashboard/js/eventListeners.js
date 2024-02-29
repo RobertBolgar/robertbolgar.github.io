a8e8ee46c6399567a074941ebb6129d422d51645
Robert Bolgar <100732701+RobertBolgar@users.noreply.github.com>
2024-02-29 11:44:55 -0600

    Update eventListeners.js

--- dashboard/js/eventListeners.js
+++ dashboard/js/eventListeners.js
@@ -1,1 +1,11 @@
+// eventListeners.js
+import { approveUser, revokeUser } from './adminFunctions.js';
+
+export function setupEventListeners(nftContract, affiliateContract) {
+    document.getElementById('approveUserBtn').addEventListener('click', async () => {
+        // Implementation...
+    });
+
+    // Continue with other event listeners setup
+}
 
