87edfed62f20361012d5f1ae653f245662bb298d
Robert Bolgar <100732701+RobertBolgar@users.noreply.github.com>
2024-02-29 11:44:02 -0600

    Update adminFunctions.js

--- dashboard/js/adminFunctions.js
+++ dashboard/js/adminFunctions.js
@@ -1,1 +1,11 @@
+// adminFunctions.js
+export async function approveUser(nftContract, userAddress) {
+    // Implementation...
+}
+
+export async function revokeUser(nftContract, userAddress) {
+    // Implementation...
+}
+
+// Continue with other functions like approveAndRegisterAffiliate, revokeAffiliate, etc.
 
