799e6317de5eabc3b49d2e1e9456d50a11be3a61
Robert Bolgar <100732701+RobertBolgar@users.noreply.github.com>
2024-03-18 09:51:33 -0500

    Update contractInteractions.js

--- claim/js/contractInteractions.js
+++ claim/js/contractInteractions.js
@@ -35,6 +35,8 @@ export async function sendPLRTToContract(plrtAmount) {
 
 export const vestingFunctions = {
   getTeamMemberVestingDetails: async (address) => {
+    export async function getTeamMemberVestingDetails(address) {
+  console.log("contract instance:", contract);
     try {
       return await contract.getTeamMemberVestingDetails(address);
     } catch (error) {
