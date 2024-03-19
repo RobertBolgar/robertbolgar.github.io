1702eb39580ff44aa33572da866aaf5df4031f77
Robert Bolgar <100732701+RobertBolgar@users.noreply.github.com>
2024-03-18 19:36:31 -0500

    Update script.js

--- claim/script.js
+++ claim/script.js
@@ -1,12 +1,3 @@
-// Initialize Web3
-if (window.ethereum) {
-    window.web3 = new Web3(window.ethereum);
-    ethereum.enable();
-} else if (window.web3) {
-    window.web3 = new Web3(window.web3.currentProvider);
-} else {
-    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
-}
 
 
 // Contract address and ABI
